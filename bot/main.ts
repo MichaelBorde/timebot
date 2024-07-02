b1f4d5b9f6fd9767ac3a01146c9d7f9334adae38
Michael Borde <michael.borde@ubimail.fr>
2024-07-02 17:23:27 +0200

    feat: more on app

--- bot/main.ts
+++ bot/main.ts
@@ -8,7 +8,5 @@ async function main() {
       environment: "test",
     },
   });
-  bot.command("start", (ctx) => ctx.reply("<°))))><"));
-  bot.command("slap", (ctx) => ctx.reply(`${ctx.match} has been trouted.`));
   return bot.start();
 }
